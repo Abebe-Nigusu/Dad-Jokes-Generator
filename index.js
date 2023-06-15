89778872b71d0691efe18b58441cf7ee154e084e
Abebe-Nigusu <92810261+Abebe-Nigusu@users.noreply.github.com>
2023-06-15 03:21:58 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const btnElement = document.getElementById("btn");
+const jokeElement = document.getElementById("joke");
+
+const apiKey = "dkPnFi7S+5yY4qmI045/NA==1kczTXxQ7iUG4G2L";
+
+const options = {
+    method: "GET",
+    headers: {
+        "X-Api-Key": apiKey,
+    },
+};
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1"
+
+async function getJoke() {
+
+    try {
+       jokeElement.innerHTML = "Updating...";
+    btnElement.disabled = true;
+    btnElement.innerText = "Loading...";
+
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+    jokeElement.innerText = data[0].joke;
+
+    btnElement.disabled = false;
+        btnElement.innerText = "Tell me a joke"; 
+        
+    } catch (error) {
+        jokeElement.innerText = "An error happened, please try again later";
+        btnElement.disabled = false;
+        btnElement.innerText = "Tell me a joke"; 
+        console.log(error);
+
+    }
+    
+
+    
+}
+
+btnElement.addEventListener("click", getJoke);
